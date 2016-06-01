94fce5a6527fd38f04d8a036f0615fb38282ea64
yinyuan <yinyuan@chengmi.com>
2016-06-01 17:40:59 +0800

    update framework

--- app/src/main/java/com/showworld/live/DemoCache.java
+++ app/src/main/java/com/showworld/live/DemoCache.java
@@ -2,38 +2,33 @@ package com.showworld.live;
 
 import android.content.Context;
 
-import com.netease.nim.chatroom.demo.im.session.image.ImageLoaderKit;
-import com.netease.nimlib.sdk.NIMClient;
-import com.netease.nimlib.sdk.uinfo.UserService;
-import com.netease.nimlib.sdk.uinfo.model.NimUserInfo;
-
 /**
  * Created by alex on 2016/6/1.
  */
 public class DemoCache {
 
     private static Context context;
-
-    private static String account;
-
-    private static NimUserInfo userInfo;
-
-    // 图片加载、缓存与管理组件
-    private static ImageLoaderKit imageLoaderKit;
-
-    public static void clear() {
-        account = null;
-        userInfo = null;
-    }
-
-    public static String getAccount() {
-        return account;
-    }
-
-    public static void setAccount(String account) {
-        DemoCache.account = account;
-    }
-
+//
+//    private static String account;
+//
+//    private static NimUserInfo userInfo;
+//
+//    // 图片加载、缓存与管理组件
+//    private static ImageLoaderKit imageLoaderKit;
+//
+//    public static void clear() {
+//        account = null;
+//        userInfo = null;
+//    }
+//
+//    public static String getAccount() {
+//        return account;
+//    }
+//
+//    public static void setAccount(String account) {
+//        DemoCache.account = account;
+//    }
+//
     public static Context getContext() {
         return context;
     }
@@ -41,20 +36,20 @@ public class DemoCache {
     public static void setContext(Context context) {
         DemoCache.context = context.getApplicationContext();
     }
-
-    public static NimUserInfo getUserInfo() {
-        if (userInfo == null) {
-            userInfo = NIMClient.getService(UserService.class).getUserInfo(account);
-        }
-
-        return userInfo;
-    }
-
-    public static ImageLoaderKit getImageLoaderKit() {
-        return imageLoaderKit;
-    }
-
-    public static void initImageLoaderKit() {
-        imageLoaderKit = new ImageLoaderKit(context, null);
-    }
+//
+//    public static NimUserInfo getUserInfo() {
+//        if (userInfo == null) {
+//            userInfo = NIMClient.getService(UserService.class).getUserInfo(account);
+//        }
+//
+//        return userInfo;
+//    }
+//
+//    public static ImageLoaderKit getImageLoaderKit() {
+//        return imageLoaderKit;
+//    }
+//
+//    public static void initImageLoaderKit() {
+//        imageLoaderKit = new ImageLoaderKit(context, null);
+//    }
 }
---
+++ app/src/main/java/com/showworld/live/main/LiveListActivity.java
@@ -0,0 +1,20 @@
+package com.showworld.live.main;
+
+import android.os.Bundle;
+
+import com.showworld.live.R;
+import com.showworld.live.base.ui.TActivity;
+
+/**
+ * Created by alex on 2016/6/1.
+ */
+public class LiveListActivity extends TActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+}
